864ea44f7e9e9622af28119b1f9074aad86ee27e
UFO-101 <josephmiller101@gmail.com>
2024-02-20 04:50:55 +0000

    - Fix bug where same_under_knockouts wasn't testing the correct circuit. - Cache completeness PruneScores as intermediate step instead of CompletenessScores.

--- auto_circuit/metrics/completeness_metrics/same_under_knockouts.py
+++ auto_circuit/metrics/completeness_metrics/same_under_knockouts.py
@@ -10,6 +10,7 @@ from auto_circuit.prune_algos.prune_algos import PRUNE_ALGO_DICT, PruneAlgo
 from auto_circuit.tasks import TASK_DICT, Task
 from auto_circuit.types import (
     AlgoKey,
+    AlgoPruneScores,
     BatchKey,
     MaskFn,
     PruneScores,
@@ -30,49 +31,6 @@ from auto_circuit.utils.tensor_ops import (
     sample_hard_concrete,
 )
 
-CompletenessScores = Dict[int, List[Tuple[float, float]]]
-AlgoCompletenessScores = Dict[AlgoKey, CompletenessScores]
-TaskCompletenessScores = Dict[TaskKey, AlgoCompletenessScores]
-
-
-def same_under_knockouts_fig(
-    task_completeness_scores: TaskCompletenessScores,
-) -> go.Figure:
-    n_cols = len(task_completeness_scores)
-    titles = [TASK_DICT[task].name for task in task_completeness_scores.keys()]
-    fig = subplots.make_subplots(rows=1, cols=n_cols, subplot_titles=titles)
-    for col, algo_comp_scores in enumerate(task_completeness_scores.values(), start=1):
-        xs = []
-        for algo_key, completeness_scores in algo_comp_scores.items():
-            for n_knockouts, kls in completeness_scores.items():
-                new_xs = [x for x, _ in kls] + [(x + y) / 2 for x, y in kls]
-                xs.extend(new_xs)
-                scatter = go.Scatter(
-                    x=new_xs,
-                    y=[y for _, y in kls] + [(x + y) / 2 for x, y in kls],
-                    name=PRUNE_ALGO_DICT[algo_key].name,
-                    hovertext=[f"n_knockouts: {n_knockouts}"],
-                    mode="lines+markers",
-                    showlegend=(col == 1),
-                )
-                fig.add_trace(scatter, row=1, col=col)
-        # Add line y=x without changing the axis limits
-        scatter = go.Scatter(
-            x=[min(xs), max(xs)],
-            y=[min(xs), max(xs)],
-            name="y=x",
-            mode="lines",
-            line=dict(dash="dash"),
-            showlegend=(col == 1),
-        )
-        fig.add_trace(scatter, row=1, col=col)
-    fig.update_layout(
-        title="Same Under Knockouts",
-        xaxis_title="KL Div",
-        yaxis_title="Knockout KL Div",
-        width=1300,
-    )
-    return fig
 
 
 def run_same_under_knockouts(
@@ -81,9 +39,8 @@ def run_same_under_knockouts(
     learning_rate: float,
     epochs: int,
     regularize_lambda: float,
-    hard_concrete_threshold: float = 0.0,
-) -> TaskCompletenessScores:
-    task_completeness_scores: TaskCompletenessScores = {}
+) -> TaskPruneScores:
+    task_completeness_scores: TaskPruneScores = {}
     for task_key, algo_prune_scores in (task_pbar := tqdm(task_prune_scores.items())):
         task = TASK_DICT[task_key]
         # if task_key != "Docstring Token Circuit":
@@ -91,15 +48,15 @@ def run_same_under_knockouts(
         assert task.true_edge_count is not None
         true_circuit_size: int = task.true_edge_count
         task_pbar.set_description_str(f"Task: {task.name}")
-        algo_completeness_scores: AlgoCompletenessScores = {}
+        algo_completeness_scores: AlgoPruneScores = {}
         for algo_key, prune_scores in (algo_pbar := tqdm(algo_prune_scores.items())):
-            # if algo_key not in algo_keys:
-            #     print("skipping algo", algo_key)
-            #     continue
+            if algo_key not in algo_keys:
+                print("skipping algo", algo_key)
+                continue
             algo = PRUNE_ALGO_DICT[algo_key]
             algo_pbar.set_description_str(f"Algo: {algo.name}")
 
-            same_under_knockouts: CompletenessScores = run_same_under_knockout(
+            same_under_knockouts: PruneScores = run_same_under_knockout(
                 task=task,
                 algo=algo,
                 prune_scores=prune_scores,
@@ -107,7 +64,6 @@ def run_same_under_knockouts(
                 learning_rate=learning_rate,
                 epochs=epochs,
                 regularize_lambda=regularize_lambda,
-                hard_concrete_threshold=hard_concrete_threshold,
             )
             algo_completeness_scores[algo_key] = same_under_knockouts
         task_completeness_scores[task_key] = algo_completeness_scores
@@ -128,8 +84,7 @@ def run_same_under_knockout(
     epochs: int,
     regularize_lambda: float,
     mask_fn: MaskFn = "hard_concrete",
-    hard_concrete_threshold: float = 0.0,
-) -> CompletenessScores:
+) -> PruneScores:
     """
     Learn a subset of the circuit to knockout such that when the same edges are knocked
     out of the full model, the KL divergence between the circuit and the full model is
@@ -202,56 +157,7 @@ def run_same_under_knockout(
         )
         fig.show()
 
-        # Test the circuit with the knockouts
-        knockout_circuit_logprobs, knockout_model_logprobs = [], []
-        circuit_logprobs, model_logprobs = [], []
-        with t.inference_mode(), mask_fn_mode(model, mask_fn=None):
-            # Discretize the circuit with knockouts
-            for circ_mask, patch_mask in zip(circ_masks, mask_params):
-                # Patch edges where learned mask is greater than hard_concrete_threshold
-                patch_mask = (patch_mask >= hard_concrete_threshold).float()
-                # Also patch edges not in the circuit
-                t.where(circ_mask, patch_mask, t.ones_like(patch_mask), out=patch_mask)
-            # Test the circuit with the knockouts
-            for batch in task.test_loader:
-                patch_outs = corrupt_src_outs_dict[batch.key].clone().detach()
-                with patch_mode(model, t.zeros_like(patch_outs), patch_outs):
-                    model_out = model(batch.clean)[model.out_slice]
-                    knockout_circuit_logprobs.append(log_softmax(model_out, dim=-1))
-
-            # Test the full model with the same knockouts
-            for circ_mask, patch_mask in zip(circ_masks, mask_params):
-                # Don't patch edges not in the circuit (but keep patches in the circuit)
-                t.where(circ_mask, patch_mask, t.zeros_like(patch_mask), out=patch_mask)
-            knockouts_size = int(sum([mask.sum().item() for mask in mask_params]))
-            for batch in task.test_loader:
-                patch_outs = corrupt_src_outs_dict[batch.key].clone().detach()
-                with patch_mode(model, t.zeros_like(patch_outs), patch_outs):
-                    model_out = model(batch.clean)[model.out_slice]
-                    knockout_model_logprobs.append(log_softmax(model_out, dim=-1))
-
-            # Test the circuit without knockouts (with tree patching)
-            for circuit_mask, patch_mask in zip(circ_masks, mask_params):
-                # Patch every edge not in the circuit
-                patch_mask = (~circuit_mask.bool()).float()
-            for batch in task.test_loader:
-                patch_outs = corrupt_src_outs_dict[batch.key].clone().detach()
-                with patch_mode(model, t.zeros_like(patch_outs), patch_outs):
-                    model_out = model(batch.clean)[model.out_slice]
-                    circuit_logprobs.append(log_softmax(model_out, dim=-1))
-
-            # Test the full model without knockouts
-            for batch in task.test_loader:
-                model_out = model(batch.clean)[model.out_slice]
-                model_logprobs.append(log_softmax(model_out, dim=-1))
-
-    knockout_kl = multibatch_kl_div(
-        t.cat(knockout_circuit_logprobs), t.cat(knockout_model_logprobs)
-    ).item()
-    normal_kl = multibatch_kl_div(t.cat(circuit_logprobs), t.cat(model_logprobs)).item()
-    print("model n_edges", model.n_edges, "knockouts_size", knockouts_size)
-    print("circuit_size", circuit_size, "diff", circuit_size - knockouts_size)
-    print("knockout_kl", knockout_kl)
-    print("normal_kl", normal_kl)
-
-    return {knockouts_size: [(normal_kl, knockout_kl)]}
+    completeness_prune_scores: PruneScores = {}
+    for mod_name, patch_mask in model.patch_masks.items():
+        completeness_prune_scores[mod_name] = patch_mask.detach().clone()
+    return completeness_prune_scores
--- auto_circuit/uniqueness_experiments.py
+++ auto_circuit/uniqueness_experiments.py
@@ -5,9 +5,10 @@ from typing import List
 import torch as t
 
 from auto_circuit.metrics.completeness_metrics.same_under_knockouts import (
+    TaskCompletenessScores,
     run_same_under_knockouts,
+    same_under_knockouts_fig,
 )
-from auto_circuit.metrics.completeness_metrics.test_same_under_knockouts import TaskCompletenessScores, same_under_knockouts_fig, test_same_under_knockout, test_completeness
 from auto_circuit.metrics.official_circuits.measure_roc import measure_roc
 from auto_circuit.metrics.official_circuits.roc_plot import roc_plot
 from auto_circuit.metrics.prune_metrics.measure_prune_metrics import (
@@ -52,11 +53,12 @@ from auto_circuit.types import (
 from auto_circuit.utils.misc import load_cache, repo_path_to_abs_path, save_cache
 from auto_circuit.utils.tensor_ops import prune_scores_threshold
 from auto_circuit.visualize import draw_seq_graph
+import plotly.graph_objects as go
 
 TASKS: List[Task] = [
     # Token Circuits
     # SPORTS_PLAYERS_TOKEN_CIRCUIT_TASK,
-    # IOI_TOKEN_CIRCUIT_TASK,
+    IOI_TOKEN_CIRCUIT_TASK,
     DOCSTRING_TOKEN_CIRCUIT_TASK,
     # Component Circuits
     # SPORTS_PLAYERS_COMPONENT_CIRCUIT_TASK,
@@ -69,83 +71,31 @@ TASKS: List[Task] = [
     # ANIMAL_DIET_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK,
     # CAPITAL_CITIES_PYTHIA_70M_AUTOENCODER_COMPONENT_CIRCUIT_TASK,
 ]
+figs: List[go.Figure] = []
 
-PRUNE_ALGOS: List[PruneAlgo] = [
-    GROUND_TRUTH_PRUNE_ALGO,
-    # ACT_MAG_PRUNE_ALGO,
-    RANDOM_PRUNE_ALGO,
-    # EDGE_ATTR_PATCH_PRUNE_ALGO,
-    # ACDC_PRUNE_ALGO,
-    INTEGRATED_EDGE_GRADS_LOGIT_DIFF_PRUNE_ALGO,
-    # LOGPROB_GRAD_PRUNE_ALGO,
-    # LOGPROB_DIFF_GRAD_PRUNE_ALGO,
-    LOGIT_DIFF_GRAD_PRUNE_ALGO,  # Fast implementation of Edge Attribution Patching
-    # LOGIT_MSE_GRAD_PRUNE_ALGO,
-    # SUBNETWORK_EDGE_PROBING_PRUNE_ALGO,
-    # CIRCUIT_PROBING_PRUNE_ALGO,
-    SUBNETWORK_TREE_PROBING_PRUNE_ALGO,
-    CIRCUIT_TREE_PROBING_PRUNE_ALGO,
-    # MSE_CIRCUIT_TREE_PROBING_PRUNE_ALGO,
-]
+# --------------------------------- Load Prune Scores ----------------------------------
 
-PRUNE_METRICS: List[PruneMetric] = [
-    CLEAN_KL_DIV_METRIC,
-    CORRUPT_KL_DIV_METRIC,
-    ANSWER_PROB_METRIC,
-    ANSWER_LOGIT_METRIC,
-    WRONG_ANSWER_LOGIT_METRIC,
-    LOGIT_DIFF_METRIC,
-    LOGIT_DIFF_PERCENT_METRIC,
-]
-figs = []
+# 2000 epoch IOI Docstring tensor prune_scores post-kv-cache-fix
+# batch_size=128, batch_count=2, default seed (for both)
+filename = "task-prune-scores-16-02-2024_23-27-49.pkl"
 
-# ------------------------------------ Prune Scores ------------------------------------
-
-compute_prune_scores = False
-save_prune_scores = False
-load_prune_scores = True
-
-task_prune_scores: TaskPruneScores = defaultdict(dict)
+# 1000 epoch Sport Players tensor prune_scores post-kv-cache-fix
+# batch_size=(10, 20), batch_count=(10, 5), default seed
+# filename = "task-prune-scores-16-02-2024_22-22-43.pkl"
 cache_folder_name = ".prune_scores_cache"
-if compute_prune_scores:
-    task_prune_scores = run_prune_algos(TASKS, PRUNE_ALGOS)
-if load_prune_scores:
-    # 2000 epoch IOI Docstring tensor prune_scores post-kv-cache-fix
-    # batch_size=128, batch_count=2, default seed (for both)
-    filename = "task-prune-scores-16-02-2024_23-27-49.pkl"
-
-    # 1000 epoch Sport Players tensor prune_scores post-kv-cache-fix
-    # batch_size=(10, 20), batch_count=(10, 5), default seed
-    # filename = "task-prune-scores-16-02-2024_22-22-43.pkl"
-
-    loaded_cache = load_cache(cache_folder_name, filename)
-    task_prune_scores = {k: v | task_prune_scores[k] for k, v in loaded_cache.items()}
-if save_prune_scores:
-    base_filename = "task-prune-scores"
-    save_cache(task_prune_scores, cache_folder_name, base_filename)
-
-for task, algo_prune_scores in task_prune_scores.items():
-    for algo, prune_scores in algo_prune_scores.items():
-        for module_name, scores in prune_scores.items():
-            # Convert dtype to float32
-            task_prune_scores[task][algo][module_name] = scores.float()
-
-for task_key in list(task_prune_scores.keys()):
-    if not task_key in [DOCSTRING_TOKEN_CIRCUIT_TASK.key]:
-        del task_prune_scores[task_key]
+task_prune_scores = load_cache(cache_folder_name, filename)
 
 # -------------------------------- Draw Circuit Graphs ---------------------------------
 
-if False:
+if True:
     for task_key, algo_prune_scores in task_prune_scores.items():
         # if not task_key.startswith("Docstring"):
         #     continue
         task = TASK_DICT[task_key]
-        if task.key != SPORTS_PLAYERS_TOKEN_CIRCUIT_TASK.key or task.true_edge_count is None:
+        if task.key != IOI_TOKEN_CIRCUIT_TASK.key or task.true_edge_count is None:
             continue
         for algo_key, ps in algo_prune_scores.items():
             algo = PRUNE_ALGO_DICT[algo_key]
-            # keys = [GROUND_TRUTH_PRUNE_ALGO.key, CIRCUIT_TREE_PROBING_PRUNE_ALGO.key]
             keys = [GROUND_TRUTH_PRUNE_ALGO.key]
             if not algo_key in keys:
                 continue
@@ -164,45 +114,11 @@ if False:
 
 # ------------------------------ Prune Scores Similarity -------------------------------
 
-if False:
+if True:
     prune_scores_similartity_fig = prune_score_similarities_plotly(
         task_prune_scores, [], ground_truths=True
     )
     figs.append(prune_scores_similartity_fig)
-
-# ------------------------------------ Completeness ------------------------------------
-
-compute_task_completeness_scores = True
-save_task_completeness_scores = False
-load_task_completeness_scores = False
-completeness_prune_scores: TaskPruneScores = {}
-algo_keys = ["Official Circuit", "Random"]
-
-if compute_task_completeness_scores:
-    completeness_prune_scores: TaskPruneScores = run_same_under_knockouts(
-        task_prune_scores,
-        algo_keys=algo_keys,
-        learning_rate=0.1,
-        epochs=50,
-        regularize_lambda=0,
-    )
-
-cache_folder_name = ".completeness_scores"
-if save_task_completeness_scores:
-    base_filename = "task-completeness-prune-scores"
-    save_cache(completeness_prune_scores, cache_folder_name, base_filename)
-
-if load_task_completeness_scores:
-    completeness_prune_scores = load_cache(cache_folder_name, filename)
-
-if completeness_prune_scores:
-    task_completeness_scores: TaskCompletenessScores = test_completeness(
-        task_prune_scores=task_prune_scores,
-        knockout_prune_scores=completeness_prune_scores,
-        algo_keys=algo_keys,
-    )
-    completeness_fig = same_under_knockouts_fig(task_completeness_scores)
-    figs.append(completeness_fig)
 
 # ----------------------------- Opposite Task Prune Scores -----------------------------
 
@@ -254,38 +170,6 @@ if roc_measurements:
     roc_fig = roc_plot(roc_measurements)
     figs.append(roc_fig)
 
-
-# ----------------------------- Prune Metric Measurements ------------------------------
-
-compute_prune_metric_measurements = False
-save_prune_metric_measurements = False
-load_prune_metric_measurements = False
-
-cache_folder_name = ".measurement_cache"
-prune_metric_measurements = None
-if compute_prune_metric_measurements:
-    prune_metric_measurements = measure_prune_metrics(
-        PRUNE_METRICS,
-        task_prune_scores,
-        PatchType.TREE_PATCH,
-        reverse_clean_corrupt=False,
-    )
-    if save_prune_metric_measurements:
-        base_filename = "seq-circuit"
-        save_cache(prune_metric_measurements, cache_folder_name, base_filename)
-if load_prune_metric_measurements:
-    # 2000 epoch IOI Docstring tensor prune_scores post-kv-cache-fix
-    # filename = "seq-circuit-17-02-2024_03-34-58.pkl"
-    # filename = "seq-circuit-18-02-2024_17-20-57.pkl"
-
-    # 1000 epoch Sport Players tensor prune_scores post-kv-cache-fix
-    # batch_size=(10, 20), batch_count=(10, 5), default seed
-    filename="seq-circuit-18-02-2024_22-09-26.pkl"
-
-    prune_metric_measurements = load_cache(cache_folder_name, filename)
-
-if prune_metric_measurements is not None:
-    figs += list(measurement_figs(prune_metric_measurements))
 
 # -------------------------------------- Figures ---------------------------------------
 
